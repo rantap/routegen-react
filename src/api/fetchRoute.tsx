9c3f6e2e6f6ef46d332374381664d7683ade648e
Petri <peewter@gmail.com>
2025-05-26 17:43:34 +0300

    Add route generation feature

--- src/api/fetchRoute.tsx
+++ src/api/fetchRoute.tsx
@@ -1,7 +1,12 @@
 const ORS_API_KEY = import.meta.env.VITE_ORS_API_KEY;
 
-export const fetchRoute = async (profile: string, coordinates: number[][]) => {
-  const url = `https://api.openrouteservice.org/v2/snap/${profile}/geojson`;
+export const fetchRoute = async (
+  profile: string,
+  coordinates: number[][],
+  length: number,
+  numberOfWaypoints: number,
+) => {
+  const url = `https://api.openrouteservice.org/v2/directions/${profile}/geojson`;
   const response = await fetch(url, {
     method: 'POST',
     headers: {
@@ -10,6 +15,13 @@ export const fetchRoute = async (profile: string, coordinates: number[][]) => {
     },
     body: JSON.stringify({
       coordinates: coordinates,
+      options: {
+        round_trip: {
+          length: length,
+          points: numberOfWaypoints,
+          seed: Math.floor(Math.random() * 1000),
+        },
+      },
     }),
   });
   return response.json();
--- src/components/RouteMap.tsx
+++ src/components/RouteMap.tsx
@@ -16,26 +16,9 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import Pin from '@/assets/Pin';
+import { fetchRoute } from '@/api/fetchRoute';
 
 const MAP_CENTER = { lon: 23.760830296, lat: 61.498164674 };
-
-const geojson: FeatureCollection = {
-  type: 'FeatureCollection',
-  features: [
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'LineString',
-        coordinates: [
-          [23.760830296, 61.49816467],
-          [23.750830296, 61.48816467],
-          [23.750830296, 61.47816467],
-        ],
-      },
-      properties: {},
-    },
-  ],
-};
 
 const routeLine: LineLayerSpecification = {
   id: 'route-line',
@@ -52,12 +35,17 @@ const routeLine: LineLayerSpecification = {
 };
 
 const RouteMap = () => {
+  const [route, setRoute] = useState<FeatureCollection>({
+    type: 'FeatureCollection',
+    features: [],
+  });
   const [routeLength, setRouteLength] = useState(5);
   const [markerVisible, setMarkerVisible] = useState(false);
   const [startingPoint, setStartingPoint] = useState({
     longitude: 0,
     latitude: 0,
   });
+  const [totalDistance, setTotalDistance] = useState(0);
 
   const setStartingMarker = (e: MapLayerMouseEvent) => {
     setStartingPoint({
@@ -65,6 +53,25 @@ const RouteMap = () => {
       latitude: e.lngLat.lat,
     });
     setMarkerVisible(true);
+  };
+
+  const generateRoute = async () => {
+    const length = routeLength * 1000;
+    const numberOfWaypoints = routeLength;
+    const coordinates = [[startingPoint.longitude, startingPoint.latitude]];
+    const response = await fetchRoute(
+      'foot-walking',
+      coordinates,
+      length,
+      numberOfWaypoints,
+    );
+    console.log('Route response:', response);
+    if (!response || !response.features) {
+      console.error('Invalid route response:', response);
+      return;
+    }
+    setRoute(response);
+    setTotalDistance(response.features[0].properties.summary.distance);
   };
 
   return (
@@ -81,11 +88,19 @@ const RouteMap = () => {
           />
         </div>
         <div className='margin-auto mt-2'>
-          <Button className='mr-4' variant='outline' disabled={!markerVisible}>
+          <Button
+            className='mr-4'
+            variant='outline'
+            disabled={!markerVisible}
+            onClick={generateRoute}
+          >
             Generate Route
           </Button>
           <Button variant='outline'>Clear Route</Button>
         </div>
+        <h1 className='mt-2 rounded-b-md px-3 py-1 lg:mt-0'>
+          Total distance: {(totalDistance / 1000).toFixed(2)} km
+        </h1>
       </div>
       <Map
         initialViewState={{
@@ -111,9 +126,11 @@ const RouteMap = () => {
         </Marker>
         )
         <NavigationControl position='bottom-left' />
-        <Source id='route-data' type='geojson' data={geojson}>
+        {route.features.length > 0} && (
+        <Source id='route-data' type='geojson' data={route}>
           <Layer {...routeLine} />
         </Source>
+        )
       </Map>
     </div>
   );
